964203995b8d6d1dfedecae9994f74e90f12482c
Yuvaraj S <yuvayuvaraj720444@gmail.com>
2025-01-23 22:54:08 +0530

    added file

--- 04-using-api/src/App.js
+++ 04-using-api/src/App.js
@@ -6,14 +6,7 @@ import "./App.css";
 function App() {
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
+     
       <h1>React Starter Project</h1>
       <div className="card">
         <p>
