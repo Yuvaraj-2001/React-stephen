bf778f16706a300e82aa7cedc261d62a900f9cd3
Yuvaraj S <yuvayuvaraj720444@gmail.com>
2025-01-24 19:22:01 +0530

    added css

--- 05-handle-forms-books/src/index.js
+++ 05-handle-forms-books/src/index.js
@@ -1,10 +1,6 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.js";
-
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+import React from 'react';
+import ReactDom from 'react-dom/client';
+import App from './App';
+const el = document.getElementById('root');
+const root = ReactDom.createRoot(el);
+root.render(<App/>);
