3f859240f322205b2c97618c3fe0f7b1b081e62c
Yuvaraj S <yuvayuvaraj720444@gmail.com>
2025-02-03 22:09:42 +0530

    added db

--- 15-redux-toolkit-query/src/components/AlbumsList.js
+++ 15-redux-toolkit-query/src/components/AlbumsList.js
@@ -1,5 +1,27 @@
+import { useFetchAlbumsQuery } from "../store";
+import Skeleton from "./Skeleton";
+import ExpandablePanel from "./ExpandablePanel";
+
 function AlbumsList({ user }) {
-  return <div>Albums for {user.name}</div>;
+  const {data, error, isLoading} =  useFetchAlbumsQuery(user);
+
+  console.log(data, error, isLoading);
+
+  let content;
+   if(isLoading){
+    content = <Skeleton times={6} className="h-10 w-full" />;
+   }else if(error){
+      content = <div>Error loading albums</div>
+   }else{
+    content = data.map(album => {
+      const header = <div>{album.title}</div> // Assuming userId is the user id from the album data
+      return <ExpandablePanel key={album.id} header={header}></ExpandablePanel>
+     })
+   }
+  
+  return <div>Albums for {user.name}
+    {content}
+  </div>;
 }
 
 export default AlbumsList;
